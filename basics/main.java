7753547238c56bdee50cad1a8c431e9cf6e238be
Ke7avan <kesavanvel81@gmail.com>
2025-02-16 12:48:41 +0530

    Packages and API

--- basics/main.java
+++ basics/main.java
@@ -1,11 +1,41 @@
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int[] arr = new int[5];
-        for(int i=0;i<5;i++){
-            arr[i] = Integer.parseInt(scan.nextLine());
-        };
+
+class Calculator{
+
+    private int number = 33;
+    
+    public int add(int num1, int num2){
+        return num1 + num2;
     }
+    public int sub(int num1, int num2){
+        return num1 - num2;
+    }
+    public int mul(int num1, int num2){
+        return num1 * num2;
+    }
+    public int div(int num1, int num2){
+        return num1 / num2;
+    }
+}
+
+class main {
+
+    static int num = 20;
+
+    public static String hello(){
+        return "Hello";
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(num);
+
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(1,10));
+
+        System.out.println(hello());
+
+    }
+
 }
